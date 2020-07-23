69ec256dda272f54496de6d96cf85dd3502ba442
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-07-24 00:02:19 +0800

    Create stack data structure

---
+++ data-structures/stack/stack.js
@@ -0,0 +1,13 @@
+const LinkedList = require("../linked-list/linkedList");
+
+class Stack extends LinkedList {
+  constructor() {
+    super();
+  }
+
+  push() {}
+
+  pop() {}
+}
+
+module.exports = Stack;
---
+++ data-structures/stack/stack.test.js
@@ -0,0 +1,3 @@
+describe("Stack", () => {
+  it.todo("should ");
+});
