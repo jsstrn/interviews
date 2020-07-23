78953d6d4515ffc8687e1507aa2655eda4c230f0
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-07-23 22:18:58 +0800

    Implement queue data structure

---
+++ data-structures/queue/queue.js
@@ -0,0 +1,55 @@
+const Node = require("../node/node");
+
+class Queue {
+  constructor() {
+    this._size = 0;
+    this._head = null;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get head() {
+    return this._head;
+  }
+
+  get tail() {
+    let pointer = this.head;
+    while (pointer && pointer.next !== null) {
+      pointer = pointer.next;
+    }
+    return pointer;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this._head = node;
+      this._size += 1;
+      return;
+    }
+
+    this.tail._next = node;
+    this._size += 1;
+  }
+
+  dequeue() {
+    if (!this.head) {
+      return false;
+    }
+
+    if (this.head && !this.head.next) {
+      this._head = null;
+      this._size -= 1;
+      return true;
+    }
+
+    this._head = this.head.next;
+    this._size -= 1;
+    return true;
+  }
+}
+
+module.exports = Queue;
---
+++ data-structures/queue/queue.test.js
@@ -0,0 +1,85 @@
+const Queue = require("./queue");
+
+describe("Queue", () => {
+  describe("init()", () => {
+    it("returns size as zero", () => {
+      const queue = new Queue();
+
+      expect(queue.size).toBe(0);
+    });
+
+    it("returns head as null", () => {
+      const queue = new Queue();
+
+      expect(queue.head).toBe(null);
+    });
+
+    it("returns tail as null", () => {
+      const queue = new Queue();
+
+      expect(queue.tail).toBe(null);
+    });
+  });
+
+  describe("enqueue()", () => {
+    it("adds a node to the queue", () => {
+      const queue = new Queue();
+
+      queue.enqueue(11);
+
+      expect(queue.size).toBe(1);
+      expect(queue.head.value).toBe(11);
+    });
+
+    it("adds two nodes to the queue", () => {
+      const queue = new Queue();
+
+      queue.enqueue(11);
+      queue.enqueue(22);
+
+      expect(queue.size).toBe(2);
+      expect(queue.head.value).toBe(11);
+      expect(queue.tail.value).toBe(22);
+    });
+
+    it("adds three nodes to the queue", () => {
+      const queue = new Queue();
+
+      queue.enqueue(11);
+      queue.enqueue(22);
+      queue.enqueue(33);
+
+      expect(queue.size).toBe(3);
+      expect(queue.head.value).toBe(11);
+      expect(queue.tail.value).toBe(33);
+    });
+  });
+
+  describe("dequeue()", () => {
+    it("returns false if there is nothing to dequeue", () => {
+      const queue = new Queue();
+
+      expect(queue.dequeue()).toBe(false);
+    });
+
+    it("returns true if dequeue was successful", () => {
+      const queue = new Queue();
+
+      queue.enqueue(11);
+
+      expect(queue.dequeue()).toBe(true);
+      expect(queue.size).toBe(0);
+    });
+
+    it("points to the next node as the new head", () => {
+      const queue = new Queue();
+
+      queue.enqueue(11);
+      queue.enqueue(22);
+      queue.dequeue();
+
+      expect(queue.head.value).toBe(22);
+      expect(queue.size).toBe(1);
+    });
+  });
+});
